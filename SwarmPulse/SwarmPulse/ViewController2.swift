10d9e6efa260dd9f1c7a2f1a36103be12fc9ee84
Lewin Koenemann <lewinkoenemann@gmail.com>
2015-10-22 14:12:56 +0200

    added second UI

--- SwarmPulse/SwarmPulse/ViewController2.swift
+++ SwarmPulse/SwarmPulse/ViewController2.swift
@@ -8,32 +8,37 @@
 import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-    @IBOutlet var noiseLable: UILabel!
+class ViewController2: UIViewController {
+    @IBOutlet var lightLabel: UILabel!
+    
+    @IBOutlet var noiseLabel: UILabel!
     
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return UIStatusBarStyle.LightContent
     }
-
+    
     override func viewDidLoad() {
         setNeedsStatusBarAppearanceUpdate()
         super.viewDidLoad()
-        _ = NSTimer.scheduledTimerWithTimeInterval(0.5, target: self, selector: Selector("updateNoiseLabel"), userInfo: nil, repeats: true)
+        _ = NSTimer.scheduledTimerWithTimeInterval(0.5, target: self, selector: Selector("updateLabel"), userInfo: nil, repeats: true)
         
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    func updateNoiseLabel()
+    func updateLabel()
     {
-        noiseLable.text = String(arc4random_uniform(49)+1) + " lux"
+        noiseLabel.text = String(arc4random_uniform(49)+1) + " dB"
+        lightLabel.text = String(arc4random_uniform(150)+1) + " lux"
+
     }
 
-
-
+    
+    
+    
 }
 
---
+++ SwarmPulse/SwarmPulse/WebViewController.swift
@@ -0,0 +1,46 @@
+//
+//  WebViewController.swift
+//  SwarmPulse
+//
+//  Created by Lewin Könemann on 22/10/15.
+//  Copyright © 2015 coss.ethz.ch. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+    
+    @IBOutlet var webView: UIWebView!
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setNeedsStatusBarAppearanceUpdate()
+        
+        let url = "http://pulse.inn.ac/"
+        let requestURL = NSURL(string:url)
+        let request = NSURLRequest(URL: requestURL!)
+        webView.loadRequest(request)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
