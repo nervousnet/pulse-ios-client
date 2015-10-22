573887d8424ad5dab6446a236a64d11dce9beb2b
Lewin Koenemann <lewinkoenemann@gmail.com>
2015-10-22 17:31:08 +0200

    working version

---
+++ SwarmPulse/SwarmPulseMessaging/ShareViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ShareViewController.swift
+//  SwarmPulseMessaging
+//
+//  Created by Lewin Könemann on 22/10/15.
+//  Copyright © 2015 coss.ethz.ch. All rights reserved.
+//
+
+import UIKit
+import Social
+
+class ShareViewController: SLComposeServiceViewController {
+
+    override func isContentValid() -> Bool {
+        // Do validation of contentText and/or NSExtensionContext attachments here
+        return true
+    }
+
+    override func didSelectPost() {
+        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+    
+        let sharedDefaults = NSUserDefaults(suiteName: "group.ch.ethz.coss.nervous")
+        sharedDefaults?.setObject(self.contentText, forKey: "stringKey")
+        sharedDefaults?.synchronize()
+        
+        
+        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+        self.extensionContext!.completeRequestReturningItems([], completionHandler: nil)
+    }
+
+    override func configurationItems() -> [AnyObject]! {
+        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+        return []
+    }
+
+}
