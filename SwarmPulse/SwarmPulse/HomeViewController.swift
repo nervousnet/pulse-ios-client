bc2314f354f752a86d5b7dce5e7e30f4ca8f3531
Lewin Koenemann <lewinkoenemann@gmail.com>
2015-10-23 15:13:00 +0200

    table view coming

---
+++ SwarmPulse/SwarmPulse/HomeViewController.swift
@@ -0,0 +1,37 @@
+//
+//  HomeViewController.swift
+//  SwarmPulse
+//
+//  Created by Lewin Könemann on 23/10/15.
+//  Copyright © 2015 coss.ethz.ch. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet var table: HomeTable!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
